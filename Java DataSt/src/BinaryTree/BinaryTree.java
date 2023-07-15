15b502caf89013aebd2a220db5ab5db6dbdea45f
raghul_r <raghulramar0811@gmail.com>
2023-07-15 14:19:01 +0530

    previous one was false now itera in-ord trav is added

--- Java DataSt/src/BinaryTree/BinaryTree.java
+++ Java DataSt/src/BinaryTree/BinaryTree.java
@@ -52,11 +52,23 @@ public class BinaryTree {
         }
     }
 
-    public void interativeInOrederTraversal(TreeNode root){
-        if(root != null){
-
-
+    public void iterativeInOrderTraversal(TreeNode root){
+        System.out.println("Iterative-In-Order Traversal of Binary Tree");
+        if(root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+        while(!stack.isEmpty() || temp != null){
+            if(temp != null){
+                stack.push(temp);
+                temp = temp.left;
+            }else{
+                temp = stack.pop();
+                System.out.print(temp.data +" ");
+                temp = temp.right;
+            }
         }
+        System.out.println();
+
     }
 
 
